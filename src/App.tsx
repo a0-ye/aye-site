6de9ac6bf8245e6c79113f771d8c7c789272072d
Adrian Y. <acye@ucsd.edu>
2025-10-02 16:29:21 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import PageCard from './assets/components/PageCard/PageCard'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [activeCard, setActiveCard] = useState(-1)
+
+
+  return (
+    <>
+      <div className='nav'>
+        <button onClick={ () => setActiveCard(-1)}>RESET</button>
+        {/* <button>projects</button>
+        <button>aboutme</button>
+        <button>contact</button> */}
+      </div>
+      
+      <div>
+        <PageCard content={<div> Hello World!</div>}    id={0} expanded={activeCard === 0}
+          onClick={() => setActiveCard(0)}
+        />
+        <PageCard content={<div> This is my life</div>} id={1} expanded={activeCard === 1}
+          onClick={() => setActiveCard(1)}
+        />
+        <PageCard content={<div> dang it sucks</div>}   id={2} expanded={activeCard === 2}
+          onClick={() => setActiveCard(2)}
+        />
+      </div>
+      <h1></h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/PageCard/PageCard.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import './PageCard.css'
+
+interface PageCardProps {
+    id: number;
+    expanded: boolean;  // 0 or 1
+    content: React.ReactNode;   // html content
+    onClick?: () => void;
+}
+
+const PageCard = (props: PageCardProps) => {
+    if(props.expanded){
+
+        return (
+            <div className='container.grown'>
+
+            </div>
+
+        )
+
+    } else{
+        return (
+                <div 
+                    className='container'
+                    onClick = {props.onClick}
+                >
+                    click me! {props.id}
+                </div>
+            )
+    }
+    
+}
+
+export default PageCard
+
+
+
+// interface ExpandingCardsProps{
+//     data: Array<PageCard>;
+// }
+
+// const ExpandingCards = ({data}: ExpandingCardsProps) => {
+
+//     return <div>{data}</div>
+// }
