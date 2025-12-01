df72a66af5088308b814a06dbb637bd6e07e32e6
Adrian Y. <acye@ucsd.edu>
2025-12-01 17:28:16 -0600

    WORKING! Refactored Drag to actually use motion drag instead of custom handler

--- src/assets/components/framerCard/MotionCard.tsx
+++ src/assets/components/framerCard/MotionCard.tsx
@@ -1,38 +1,9 @@
 import type { UniqueIdentifier } from "@dnd-kit/core"
-import { frame, motion, useAnimate, useSpring } from "motion/react"
-import { useEffect, useState, type CSSProperties, type ReactNode, type RefObject} from "react"
+import { frame, motion, useAnimate, useMotionValue, useSpring } from "motion/react"
+import { useEffect, useRef, useState, type CSSProperties, type ReactNode, type RefObject} from "react"
 import { makeCoords, type CardData, type coord} from "../../../App"
 import Draggable from "../dnd-kit-wrappers/draggable"
 
-
-function useFollowPointer(ref: RefObject<HTMLDivElement | null>, doFollow: boolean) {
-    const spring = { damping: 8, stiffness: 120, mass: 0.01, restDelta: 0.001 }
-    const x = useSpring(0, spring)
-    const y = useSpring(0, spring)
-    useEffect(() => {
-        if (!ref || !ref.current) return
-        if (!doFollow) return
-
-        const handlePointerMove = (e: PointerEvent) => {
-            const { clientX, clientY } = e
-            const element = ref.current;
-            const rect = ref.current?.getBoundingClientRect();
-            if (!element || !rect) return
-
-            frame.read(() => {
-                x.set(clientX - rect.left - element.offsetWidth / 2) // problem: element.offsetLeft is 0 compared to its parent
-                y.set(clientY - rect.top - element.offsetHeight / 2) //            need to f
-                console.log(x.get(), y.get());
-                
-            })
-        }
-        window.addEventListener("pointermove", handlePointerMove)
-        return () => {
-            window.removeEventListener("pointermove", handlePointerMove)
-        }
-    }, [doFollow, ref, x,y])
-    return {x,y}
-}
 
 interface CardProps{
     cardData:CardData
@@ -48,33 +19,49 @@ interface CardProps{
 export default function MotionCard(props: CardProps) {
     const cardData = props.cardData
     const [ref, animate] = useAnimate();
-    const [doFollow, setFollow] = useState(false)
-    const {x,y} = useFollowPointer(ref, doFollow)
+    const springConfig = { damping: 8, stiffness: 120, mass: 0.01, restDelta: 0.001 }
+
+    const targetX = useMotionValue(cardData.origin.x);
+    const targetY = useMotionValue(cardData.origin.y);
+    const currentX = useSpring(targetX, springConfig);
+    const currentY = useSpring(targetY, springConfig);
+
+    // const onDragPosition = useRef({x:0, y:0})
+
 
     const returnToOrigin = () => { 
-        setFollow(false);
         animate(ref.current, makeCoords(cardData.origin.x, cardData.origin.y))
     }
 
     useEffect(()=>{
-        console.log("Origin Updated!!");
-        returnToOrigin()
-    },[cardData.origin.x, cardData.origin.y]
-    )    // when origin is updated, return to origin
-    // window.addEventListener("pointerup",returnToOrigin);
+            returnToOrigin()
+        },[cardData.origin.x, cardData.origin.y]
+    )
 
     return <motion.div
-        // drag
-        onMouseDown={()=> {
-                setFollow(true)
-            }
-        }
-        // onDragEnd={()=>{
-        //     console.log("drag ended");
-        //     returnToOrigin()}}
         ref={ref}
-        style={{...box, x,y, ...props.style, 
-                originX:cardData.origin.x, originY:cardData.origin.y}}>
+        drag
+        onDragStart={()=>{
+            // onDragPosition.current.x = currentX.get()
+            // onDragPosition.current.y = currentY.get()
+        }}
+        onDrag={(_,info)=>{
+            const newTargetX = cardData.origin.x + info.offset.x
+            const newTargetY = cardData.origin.y + info.offset.y
+            targetX.set(newTargetX)
+            targetY.set(newTargetY)
+        }}
+
+        onDragEnd={()=>{
+            console.log("drag ended");
+            returnToOrigin()}}
+
+        
+        style={{
+            x:currentX, y:currentY, // controls the position of the card. Uses currentX and currentY to spring towards targetXY
+                ...box, ...props.style, 
+                // originX:cardData.origin.x, originY:cardData.origin.y
+                }}>
             <Draggable drag_id={cardData.id}/>
             
             <p>ID: {cardData.id}</p>
